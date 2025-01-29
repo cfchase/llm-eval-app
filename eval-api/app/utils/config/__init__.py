487f7121bcd44b7fada477dcefdf9eb619262aec
Chris Chase <cchase@redhat.com>
2025-01-29 15:08:40 -0500

    Switched to kustomize templates for deployment

--- eval-api/app/utils/config/__init__.py
+++ eval-api/app/utils/config/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import logging
 import yaml
@@ -7,7 +8,7 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
-APP_CONFIG_PATH = os.environ.get("APP_CONFIG_PATH", "app_config.yaml")
+APP_CONFIG_PATH = os.environ.get("APP_CONFIG_PATH")
 
 
 def load_yaml_config(filepath):
@@ -24,10 +25,17 @@ def load_base64_yaml_config(filepath):
     return config
 
 
-if APP_CONFIG_PATH.endswith(".yaml") and os.path.exists(APP_CONFIG_PATH):
+if APP_CONFIG_PATH and APP_CONFIG_PATH.endswith(".yaml") and os.path.exists(APP_CONFIG_PATH):
     app_config = load_yaml_config("app_config.yaml")
-elif APP_CONFIG_PATH.endswith(".yaml.b64") and os.path.exists(APP_CONFIG_PATH):
+elif APP_CONFIG_PATH and APP_CONFIG_PATH.endswith(".yaml.b64") and os.path.exists(APP_CONFIG_PATH):
     app_config = load_base64_yaml_config("app_config.yaml.b64")
+else:
+    app_config = load_yaml_config("app_config_default.yaml")
+    app_config["judge"]["api_key"] = os.environ["OPENAI_API_KEY"]
+    app_config["comparison_configs"][0]["endpoint_url"] = os.environ["GRANITE_ENDPOINT_URL"]
+    app_config["comparison_configs"][0]["model_name"] = os.environ["GRANITE_MODEL_NAME"]
+    app_config["comparison_configs"][0]["api_key"] = os.environ["GRANITE_API_KEY"]
+    app_config["comparison_configs"][1]["api_key"] = os.environ["OPENAI_API_KEY"]
 
 
 if app_config and app_config.get("judge") and app_config["judge"].get("api_key"):
